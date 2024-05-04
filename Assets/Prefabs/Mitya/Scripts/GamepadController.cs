c75c50633a71abb80fa4dd3e0d3340d3312c7a9a
DmitryDzz <DzakhovDS@gmail.com>
2024-05-04 23:45:35 +0700

    GamepadController

---
+++ Assets/Prefabs/Mitya/Scripts/GamepadController.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+// ReSharper disable once CheckNamespace
+namespace Prefabs.Mitya.Scripts
+{
+    public class GamepadController : MonoBehaviour
+    {
+        public MoveController moveController;
+        
+        private InputControls _inputControls;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(moveController);
+            _inputControls = new InputControls();
+        }
+
+        private void OnEnable()
+        {
+            _inputControls.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputControls.Disable();
+        }
+
+        private void Update()
+        {
+            bool fastRotation = _inputControls.TankActionMap.RotationMode.IsPressed();
+            float triggerSpeedFactor = fastRotation ? 1f : _inputControls.TankActionMap.SpeedFactor.ReadValue<float>();
+            Vector2 moveVector = CircleToSquare(_inputControls.TankActionMap.Move.ReadValue<Vector2>());
+            float leftSpeedFactor = fastRotation
+                ? moveVector.x / 2f
+                : moveVector.x < 0f ? 1f + moveVector.x : 1f;
+            float rightSpeedFactor = fastRotation
+                ? -moveVector.x / 2f
+                : moveVector.x > 0f ? 1f - moveVector.x : 1f;
+            float directionSpeedFactor = fastRotation ? 1f : moveVector.y;
+
+            float leftSpeed = triggerSpeedFactor * directionSpeedFactor * leftSpeedFactor * moveController.maxSpeed;
+            float rightSpeed = triggerSpeedFactor * directionSpeedFactor * rightSpeedFactor * moveController.maxSpeed;
+            moveController.speedFL = leftSpeed;
+            moveController.speedRL = leftSpeed;
+            moveController.speedFR = rightSpeed;
+            moveController.speedRR = rightSpeed;
+        }
+        
+        private static Vector2 CircleToSquareInFirstQuadrant(Vector2 value)
+        {
+            float x = value.x;
+            float y = value.y;
+            if (x == 0) return value; // (to avoid dividing by 0)
+            if (x >= 0 && y >= 0)
+            {
+                bool firstOctantInQuadrant = x >= y;
+                if (!firstOctantInQuadrant) (x, y) = (y, x);
+                float resultX = Mathf.Sqrt(x * x + y * y);
+                float resultY = y * resultX / x;
+                value = new Vector2(resultX, resultY);
+                if (!firstOctantInQuadrant) (value.x, value.y) = (value.y, value.x);
+            }
+            return value;
+        }
+
+        private static Vector2 CircleToSquare(Vector2 value)
+        {
+            if (value is {x: >= 0, y: >= 0})
+            {
+                value = CircleToSquareInFirstQuadrant(value);
+            }
+            else if (value is {x: < 0, y: >= 0})
+            {
+                value.x = -value.x;
+                value = CircleToSquareInFirstQuadrant(value);
+                value.x = -value.x;
+            }
+            else if (value is {x: < 0, y: < 0})
+            {
+                value.x = -value.x;
+                value.y = -value.y;
+                value = CircleToSquareInFirstQuadrant(value);
+                value.x = -value.x;
+                value.y = -value.y;
+            }
+            else if (value is {x: >= 0, y: < 0})
+            {
+                value.y = -value.y;
+                value = CircleToSquareInFirstQuadrant(value);
+                value.y = -value.y;
+            }
+            return new Vector2(Mathf.Clamp(value.x, -1f, 1f), Mathf.Clamp(value.y, -1f, 1f));
+        }
+
+    }
+}
--- Assets/Prefabs/Mitya/Scripts/MoveController.cs
+++ Assets/Prefabs/Mitya/Scripts/MoveController.cs
@@ -6,29 +6,16 @@ namespace Prefabs.Mitya.Scripts
 {
     public class MoveController : MonoBehaviour
     {
-        public Transform tempPoint;
         [Header("Links")]
         public WheelController wheelController;
-        public Transform robotMoveModel;
-        public Transform robotRotateModel;
-
-        [Header("Move Geometry")]
-        [Tooltip("Local vector")]
-        public Vector3 moveRightVector = Vector3.right;
-        [Tooltip("Local vector")]
-        public Vector3 moveUpVector = Vector3.up;
-        [Tooltip("Local vector")]
-        public Vector3 moveForwardVector = Vector3.forward;
+        public Transform robotTransform;
+        // public Transform tempPoint;
         
-        [Header("Rotate Geometry")]
+        [Header("Robot specs")]
         public float robotBaseWidth = 0.26f;
         // public float robotBaseLength = 0.24f;
-        [Tooltip("Local vector")]
-        public Vector3 rotateRightVector = Vector3.right;
-        [Tooltip("Local vector")]
-        public Vector3 rotateUpVector = Vector3.up;
-        [Tooltip("Local vector")]
-        public Vector3 rotateForwardVector = Vector3.forward;
+        [Tooltip("Max speed in meters per second")]
+        public float maxSpeed = 1.0f;
         
         [Header("Wheel Speeds")]
         [Tooltip("Wheel speed in meters per second")]
@@ -49,12 +36,13 @@ namespace Prefabs.Mitya.Scripts
         private void Awake()
         {
             Assert.IsNotNull(wheelController);
-            Assert.IsNotNull(robotMoveModel);
-            Assert.IsNotNull(robotRotateModel);
+            Assert.IsNotNull(robotTransform);
         }
 
         private void Update()
         {
+            float deltaTime = Time.deltaTime;
+
             wheelController.speedFL = speedFL;
             wheelController.speedFR = speedFR;
             wheelController.speedRL = speedRL;
@@ -65,19 +53,19 @@ namespace Prefabs.Mitya.Scripts
 
             float moveLinearSpeed = GetMoveLinearSpeed(speedLeft, speedRight);
             float rotationAngularSpeed = GetRotateAngularSpeed(speedLeft, speedRight, robotBaseWidth);
-            Vector3 localRotationOrigin = GetRotateLocalOrigin(speedLeft, speedRight, rotateRightVector, robotBaseWidth);
+            
+            Vector4 localRotationOrigin = GetRotateLocalOrigin(speedLeft, speedRight, robotBaseWidth);
+            Vector3 rotationOrigin = robotTransform.localToWorldMatrix * localRotationOrigin;
+            Vector3 rotationAxis = robotTransform.localToWorldMatrix * Vector3.up;
+            robotTransform.RotateAround(rotationOrigin,  rotationAxis, rotationAngularSpeed * deltaTime);
+            
+            Vector3 localMoveVector = moveLinearSpeed * deltaTime * Vector3.forward;
+            Vector3 moveVector = robotTransform.localToWorldMatrix * localMoveVector;
+            robotTransform.position += moveVector;
 
-            float deltaTime = Time.deltaTime;
-            Vector3 localMoveVector = moveLinearSpeed * deltaTime * moveForwardVector;
-            Vector3 moveVector = robotRotateModel.localToWorldMatrix * localMoveVector;
             // Debug.DrawRay(robotMoveModel.position, moveVector.normalized, Color.red);
-            robotMoveModel.position += moveVector;
-            Vector3 rotationOrigin = robotRotateModel.localToWorldMatrix * localRotationOrigin;
-            Vector3 localRotationAxis = rotateUpVector;
-            Vector3 rotationAxis = robotRotateModel.localToWorldMatrix * localRotationAxis;
-            robotRotateModel.RotateAround(rotationOrigin,  rotationAxis, rotationAngularSpeed * deltaTime);
-            Debug.Log($"linerSpeed: {moveLinearSpeed:F2}    angularSpeed: {rotationAngularSpeed:F2}    localRotationOrigin: {localRotationOrigin}    rotationOrigin: {rotationOrigin}    localRotationAxis: {localRotationAxis}    rotationAxis: {rotationAxis}");
-            tempPoint.position = rotationOrigin;
+            // Debug.Log($"linerSpeed: {moveLinearSpeed:F2}    angularSpeed: {rotationAngularSpeed:F2}    localRotationOrigin: {localRotationOrigin}    rotationOrigin: {rotationOrigin}    rotationAxis: {rotationAxis}");
+            // tempPoint.position = rotationOrigin;
         }
 
         private static float GetSideSpeed(float speedFront, float speedRear)
@@ -94,28 +82,24 @@ namespace Prefabs.Mitya.Scripts
             return speedLeft + speedRight;
         }
 
-        // private static float GetRotateLinearSpeed(float speedLeft, float speedRight)
-        // {
-        //     // if (speedLeft >= 0f && speedRight >= 0f) return speedLeft - speedRight;
-        //     // if (speedLeft <= 0f && speedRight <= 0f) return speedLeft - speedRight;
-        //     return speedLeft - speedRight;
-        // }
-
         private static float GetRotateAngularSpeed(float speedLeft, float speedRight, float robotBaseWidth)
         {
             float rotateLinearSpeed = speedLeft - speedRight;
             if (Mathf.Approximately(robotBaseWidth, 0f)) return 0f;
             float rotateRadius = speedLeft * speedRight >= 0 ? robotBaseWidth : robotBaseWidth / 2f;
-            // Debug.Log($"{rotateLinearSpeed:F2}    {rotateRadius:F2}    {Mathf.PI * rotateRadius / rotateLinearSpeed / 180}");
             return rotateLinearSpeed / rotateRadius * RadToDeg; // degrees per second
         }
 
-        private static Vector3 GetRotateLocalOrigin(float speedLeft, float speedRight, Vector3 rightVector, float robotBaseWidth)
+        private static Vector4 GetRotateLocalOrigin(float speedLeft, float speedRight, float robotBaseWidth)
         {
+            Vector4 result;
             float r = robotBaseWidth / 2f;
-            if (speedLeft >= 0f && speedRight >= 0f) return speedRight - speedLeft >= 0 ? r * rightVector : -r * rightVector;
-            if (speedLeft <= 0f && speedRight <= 0f) return speedRight - speedLeft >= 0 ? -r * rightVector : r * rightVector;
-            return Vector3.zero;
+            Vector3 right = Vector3.right;
+            if (speedLeft >= 0f && speedRight >= 0f) result = speedRight - speedLeft >= 0 ? -r * right : r * right;
+            else if (speedLeft <= 0f && speedRight <= 0f) result = speedRight - speedLeft >= 0 ? r * right : -r * right;
+            else result = Vector3.zero;
+            result.w = 1;
+            return result;
         }
     }
 }
